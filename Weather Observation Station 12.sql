5f337d1d8d1d81d13cc92f03c16291139e8d07d3
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2025-01-14 20:54:52 +0530

    Create Weather Observation Station 12.sql

---
+++ Weather Observation Station 12.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT city
+FROM STATION
+WHERE LEFT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+OR RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+
+
+SELECT DISTINCT city
+FROM STATION
+WHERE LEFT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+OR RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
